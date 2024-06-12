71dcdf266833802aa8701e2c5948662868b5eceb
Benjamin <binemugha@gmail.com>
2024-06-12 09:37:08 +0100

    Start implementation for expense item

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,12 +1,20 @@
-class Expense {
-  const Expense(
-      {required this.id,
-      required this.title,
-      required this.amount,
-      required this.date});
+import 'package:uuid/uuid.dart';
 
-  final int id;
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+
+  final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
